f4ee74dde4589ff4e54adcd8bc5f433516831113
Long Nguyen <long@gtoken.com>
2018-11-23 10:40:11 +0700

    Initial Commit

---
+++ GitXCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXCode
+//
+//  Created by Long Nguyen on 11/23/18.
+//  Copyright © 2018 Long Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
