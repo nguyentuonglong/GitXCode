55458777ae3477e033b53deb1082fe1da3cc3e9a
Long Nguyen <long@gtoken.com>
2018-11-23 14:09:53 +0700

    Edit again

--- GitXCode/ViewController.swift
+++ GitXCode/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello Git in XCode")
-        print("Next next")
+        print("Edit this LOC againnnnnn")
     }
 
 
